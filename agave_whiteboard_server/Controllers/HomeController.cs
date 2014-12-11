ca73219ba06d98807791a1727f93266bb706f3e9
unknown <davech@DAVECHL10.redmond.corp.microsoft.com>
2014-12-10 18:32:49 -0800

    added rooms page which will list out all rooms activated

--- agave_whiteboard_server/Controllers/HomeController.cs
+++ agave_whiteboard_server/Controllers/HomeController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using agave_whiteboard_server.Models;
 
 namespace agave_whiteboard_server.Controllers
 {
     public class HomeController : Controller
     {
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            if (StaticModel.rooms==null) StaticModel.rooms = new Rooms();
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -30,13 +37,15 @@ namespace agave_whiteboard_server.Controllers
         [Route("{id:string}")]
         public ActionResult WhiteBoard(string id)
         {
+            StaticModel.rooms.AddRoom(new Room(id));
             ViewBag.Group = id;
-            return View();
+            return View(StaticModel.rooms.GetRooms());
         }
 
         public ActionResult Rooms()
         {
-            return View();
+            
+            return View(StaticModel.rooms.GetRooms());
         }
 
         public ActionResult Office()
