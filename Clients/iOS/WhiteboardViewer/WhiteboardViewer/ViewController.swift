22cdc1fe1f2ea87102fd81edc42bb83b42251a6d
johnaustin <johnaustin@microsoft.com>
2014-12-10 20:51:50 -0800

    Connected webView to the controller
    the webView widget on the storyboard is now connected to the IBOutlet
    var in the controller

--- Clients/iOS/WhiteboardViewer/WhiteboardViewer/ViewController.swift
+++ Clients/iOS/WhiteboardViewer/WhiteboardViewer/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var webView: UIWebView!
+   @IBOutlet var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         let request = NSURLRequest(URL: url!)
         
         webView.loadRequest(request)
+        
     }
 
     override func didReceiveMemoryWarning() {
