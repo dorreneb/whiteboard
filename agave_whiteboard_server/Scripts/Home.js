d39b55daee9298e848cba9fdf3ab08ff74aeb023
unknown <davech@DAVECHL10.redmond.corp.microsoft.com>
2014-12-10 22:43:19 -0800

    Fixed couple of remaining merge issues

--- agave_whiteboard_server/Scripts/Home.js
+++ agave_whiteboard_server/Scripts/Home.js
@@ -2,15 +2,15 @@
 
 (function () {
     "use strict";
-    try {
-        
+    
+    Office.initialize = function (reason) {
         $(document).ready(function () {
-           
+
             app.initialize();
             var docname;
             var loaded = false;
             var savedID = Office.context.document.settings.get("id");
-            
+
             if (!savedID) {
                 docname = encodeURI(Office.context.document.url);
             }
@@ -18,18 +18,13 @@
                 docname = savedID;
                 loaded = true;
             }
-            
-            //var destinationUrl = "https://" + window.location.hostname +"/home/whiteboard?id=" + docname + "&isJoin=" + loaded;
-            console.log("docname " + docname + "/loaded " + loaded)
-            var destinationUrl = "http://localhost:55802/home/whiteboard?isJoin=" + loaded + "&id=" + docname;
-               
-             window.location.replace(destinationUrl);  
-            
-        });
 
-    };
-    
-    } catch (err) {
-        document.write(":(");
+            //var destinationUrl = "https://" + window.location.hostname +"/home/whiteboard?id=" + docname + "&isJoin=" + loaded;
+            //console.log("docname " + docname + "/loaded " + loaded);
+            var destinationUrl = "http://localhost:55802/home/whiteboard?isJoin=" + loaded + "&id=" + docname;
+
+            window.location.replace(destinationUrl);
+
+        });
     }
 })();
