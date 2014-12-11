6d66d07c5c080ad29a5e787de9376187bddc7a21
Dorrene Brown <dobrown@microsoft.com>
2014-12-10 18:35:05 -0800

    Cleaned up JS code a little

--- agave_whiteboard_server/Scripts/whiteBoardDraw.js
+++ agave_whiteboard_server/Scripts/whiteBoardDraw.js
@@ -10,7 +10,7 @@ var drawComponents = {
 };
 
 //resize the canvas to fit all available space on the screen and nothing more
-function responsive() {
+function resizeScreen() {
     var menubarheight = $(drawComponents.menu).height();
     var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
     var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
@@ -24,7 +24,7 @@ function responsive() {
 
 //when the window resizes resize the canvas
 window.onresize = function (event) {
-    responsive();
+    resizeScreen();
 };
 
 //if draw button is selected draw a circle
@@ -46,38 +46,35 @@ $(drawComponents.selectButton).click(function () {
 //when document is actually ready do stuff
 $(document).ready(function () {
     //create the canvas
-    window.canvas = new fabric.Canvas('drawCanvas');
+    window.canvas = new fabric.Canvas(drawComponents.canvasName);
 
     //resize the window to fit the screen
-    responsive();
+    resizeScreen();
 
     //when clicking on the screen draw text when necessary
     window.canvas.on('mouse:up', function (options) {
-        if (!options.target && window.currentAction == drawComponents.drawButton) {
+        if (!options.target && window.currentAction === drawComponents.drawButton) {
             var circle = new fabric.Circle({
                 radius: 10, fill: 'green', left: options.e.clientX - 5, top: options.e.clientY - 5 - $(drawComponents.menu).height()
             });
             window.canvas.add(circle);
-            var status = JSON.stringify(window.canvas);
-            window.sync(status);
+            window.sync();
         }
     });
 
     //when clicking on the screen draw a circle
     window.canvas.on('mouse:up', function (options) {
-        if (!options.target && window.currentAction == drawComponents.textButton) {
+        if (!options.target && window.currentAction === drawComponents.textButton) {
             var circle = new fabric.Circle({
                 radius: 10, fill: 'red', left: options.e.clientX - 5, top: options.e.clientY - 5 - $(drawComponents.menu).height()
             });
             window.canvas.add(circle);
-            var status = JSON.stringify(window.canvas);
-            window.sync(status);
+            window.sync();
         }
     });
 
     window.canvas.on('object:modified', function (options) {
-        var status = JSON.stringify(window.canvas);
-        window.sync(status);
+        window.sync();
     });
 
 });
--- agave_whiteboard_server/Scripts/whiteboardConnect.js
+++ agave_whiteboard_server/Scripts/whiteboardConnect.js
@@ -11,41 +11,19 @@ $(function () {
     // Reference the auto-generated proxy for the hub.
     window.chat = $.connection.whiteBoardHub;
 
-    //function that shows someone is logging into the system
-    window.chat.client.systemMessage = function (message) {
-        $(components.chat).append('<li><strong>' + htmlEncode(message) + '</strong></li>');
-    }
-
-    // Create a function that the hub can call back to display messages.
-    window.chat.client.addNewMessageToPage = function (name, message) {
-        // Add the message to the page.
-        $(components.chat).append('<li><strong>' + htmlEncode(name) + '</strong>: ' + htmlEncode(message) + '</li>');
-    };
-
     //changes canvas after receiving updates
     window.chat.client.getSync = function (json) {
         window.canvas.loadFromJSON(json, canvas.renderAll.bind(canvas));
     }
 
     //sends the json from the canvas to all other members in the group
-    window.sync = function (json) {
-        chat.server.sync(json, $(components.group).val());
+    window.sync = function () {
+        var status = JSON.stringify(window.canvas);
+        chat.server.sync(status, $(components.group).val());
     }
 
     // Start the connection.
     $.connection.hub.start().done(function () {
         $.connection.whiteBoardHub.server.joinRoom($(components.username).val(), $(components.group).val());
-        $(components.sendmessagebutton).click(function () {
-            // Call the Send method on the hub.
-            chat.server.send($(components.username).val(), $(components.message).val(), $(components.group).val());
-            // Clear text box and reset focus for next comment.
-            $(components.message).val('').focus();
-        });
     });
 });
-// This optional function html-encodes messages for display in the page.
-function htmlEncode(value) {
-    var encodedValue = $('<div />').text(value).html();
-    return encodedValue;
-}
-
