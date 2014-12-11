80adb79eb6f0a3b502af5d3d7ef62d0d783a9fcf
unknown <davech@DAVECHL10.redmond.corp.microsoft.com>
2014-12-10 20:30:24 -0800

    now saves the session id when reloading a ppt

---
+++ agave_whiteboard_server/Scripts/HomeWB.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+          
+        });
+
+    };
+})();
