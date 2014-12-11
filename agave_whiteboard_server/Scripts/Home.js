498b791ff45bd119bc31e5ea66e443855be0373c
unknown <davech@DAVECHL10.redmond.corp.microsoft.com>
2014-12-10 17:26:07 -0800

    fixed problem with hardcoded url in redirect of home.js

--- agave_whiteboard_server/Scripts/Home.js
+++ agave_whiteboard_server/Scripts/Home.js
@@ -7,11 +7,20 @@
     Office.initialize = function (reason) {
         $(document).ready(function () {
             app.initialize();
-            var docname = encodeURI(Office.context.document.url);
+   //         var docname = encodeURI(Office.context.document.url);
             //remove the file extenstion from end
            // if (docname.length>5) docname = docname.substring(0, docname.length - 5);
-            window.location.replace("http://localhost:55802/home/whiteboard/?id="+docname);
+            //       window.location.replace("http://localhost:55802/home/whiteboard/?id="+docname);
+            
+                var docname = encodeURI(Office.context.document.url);
+                //remove the file extenstion from end
+                // if (docname.length>5) docname = docname.substring(0, docname.length - 5);
+                
+                //document.getElementById("content-main").src = "http://localhost:55802/home/whiteboard/?id=" + docname;
+                var destinationUrl = "https://" + window.location.hostname +"/home/whiteboard/?id=" +docname;
+                //var destinationUrl = window.location.protocol + "://" + window.location.hostname + ":" + window.location.port + "/home/whiteboard/?id=" + docname;
+                window.location.replace(destinationUrl);
         })
         
-        }
+    }
 })();
