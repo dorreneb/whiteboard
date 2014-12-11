477c3f3422d561af3af6ba619fe0d2e533c32dba
Dorrene Brown <dobrown@microsoft.com>
2014-12-11 00:49:45 -0800

    rooms look nice

--- agave_whiteboard_server/Scripts/whiteBoardDraw.js
+++ agave_whiteboard_server/Scripts/whiteBoardDraw.js
@@ -58,7 +58,6 @@ window.typeWords = function (text, x, y) {
 
     });
     window.canvas.add(text);
-
     window.sync();
 }
 
@@ -99,7 +98,6 @@ $(document).ready(function () {
 
             });
             window.canvas.add(text);
-
             window.sync();
         }
     });
